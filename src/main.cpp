81bb20aa763e995ff26232d08dec1c9b6e4a622f
theBashShell <edohbruno@gmail.com>
2020-01-07 18:47:13 +0000

    classes

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,24 @@
 
 using namespace std;
 
-int doubler(const int& x) {
-  return x * 2;
-}
+class Human
+{
+private:
+  string secret;
+
+public:
+  string name;
+  int age;
+  Human(const string &secret, const string &name, const int &age) : secret(secret), age(age), name(name) {}
+  ~Human()
+  {
+    cout << "Destructor called\n";
+  }
+};
 
 int main(void)
 {
-  int first = 10;
-
-  cout << doubler(first) << endl;
-  cout << doubler(20) << endl;
-}       
+  Human bruno("sersdaf", "Bruno Edoh", 232);
+  cout << bruno.name << endl;
+  cout << bruno.age << endl;
+}
