09f705a4393b7ddefbb38a3f39ffbe73e88dcd3b
theBashShell <edohbruno@gmail.com>
2020-01-07 18:54:56 +0000

    operator overloading example

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,24 @@ public:
   string name;
   int age;
   Human(const string &secret, const string &name, const int &age) : secret(secret), age(age), name(name) {}
-  ~Human()
-  {
-    cout << "Destructor called\n";
-  }
+  ~Human();
+  Human &operator+(const int &);
 };
+
+Human::~Human()
+{
+  cout << "Destructor called\n";
+}
+
+Human &Human::operator+(const int &num)
+{
+  this->age = this->age + num;
+}
 
 int main(void)
 {
   Human bruno("sersdaf", "Bruno Edoh", 232);
   cout << bruno.name << endl;
+  bruno + 20;
   cout << bruno.age << endl;
 }
