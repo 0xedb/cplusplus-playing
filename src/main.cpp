3f089aed7510b45a9be6dad48477c25bbf831861
theBashShell <edohbruno@gmail.com>
2020-02-07 01:40:19 +0000

    linked list

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,46 @@
 
 using namespace std;
 
-void callback(int x, int (*fn)(int)) {
-  cout << fn(x) << endl;
+struct Node
+{
+  int data;
+  Node *next;
+};
+
+class SinglyLinkedList
+{
+private:
+  Node *head = nullptr;
+
+public:
+  void add_to_front(int);
+  void traverse(void);
+};
+
+void SinglyLinkedList::add_to_front(int data)
+{
+  Node *latest = new Node;
+  latest->data = data;
+
+  latest->next = head;
+  head = latest;
 }
 
-int func(int b) {
-  return b * 2;
+void SinglyLinkedList::traverse()
+{
+  Node *current = head;
+  while (current)
+  {
+    cout << current->data << endl;
+    current = current->next;
+  }
 }
 
-int main(void) { 
-  callback(20, func);
-  return 0;
+int main(void)
+{
+  SinglyLinkedList ll;
+  ll.add_to_front(1);
+  ll.add_to_front(30);
+  ll.add_to_front(29);
+  ll.traverse();
 }
